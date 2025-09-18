0c55e8cc8cf98cba3f18d4f552b087a6f6a183ff
Zarema Khalilova <zarema.khalilova@gmail.com>
2025-09-18 07:50:33 +0400

    fix typescript

--- packages/shared-components/src/index.ts
+++ packages/shared-components/src/index.ts
@@ -1,3 +1,16 @@
 // Export shared components
 export { default as Time } from "./components/Time.astro";
 export { default as RawVideo } from "./components/RawVideo.astro";
+
+// Type declarations for TypeScript
+export interface TimeProps {
+  datetime: Date;
+}
+
+export interface RawVideoProps {
+  src: string;
+  type?: string;
+  poster?: string;
+  width?: number;
+  height?: number;
+}
