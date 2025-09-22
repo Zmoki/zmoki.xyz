b505624421ff4a825cb69f5b4c82ad6490fa13f1
Zarema Khalilova <zarema.khalilova@gmail.com>
2025-09-22 16:36:27 +0400

    add brevo form component

--- apps/zmoki-pages/src/env.d.ts
+++ apps/zmoki-pages/src/env.d.ts
@@ -1,1 +1,9 @@
 /// <reference path="../.astro/types.d.ts" />
+interface ImportMetaEnv {
+  readonly PUBLIC_CLOUDFLARE_TURNSTILE_SITE_KEY: string;
+  readonly PUBLIC_BREVO_ACCOUNT_ID: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
