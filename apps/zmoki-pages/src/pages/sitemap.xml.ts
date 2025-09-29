92771ddabb2cca2e194ee596512d857e7674b5f8
Zarema Khalilova <zarema.khalilova@gmail.com>
2025-09-29 16:04:16 +0400

    update sitemap

--- apps/zmoki-pages/src/pages/sitemap.xml.ts
+++ apps/zmoki-pages/src/pages/sitemap.xml.ts
@@ -1,5 +1,5 @@
 import type { APIRoute } from "astro";
-import { getCollection } from "astro:content";
+import { getCollection, type CollectionEntry } from "astro:content";
 import {
   publishDate as indexPagePublishDate,
   contentModifiedDate as indexPageContentModifiedDate,
@@ -7,15 +7,18 @@ import {
 
 export const GET: APIRoute = async ({ site }) => {
   // Get all posts from the feed collection
-  const allFeedIems = await getCollection("feed");
+  const allFeedIems: CollectionEntry<"feed">[] = await getCollection("feed");
 
-  // Sort posts by publish date (newest first)
-  const sortedFeedItems = allFeedIems.sort(
-    (a, b) => b.data.publishDate.getTime() - a.data.publishDate.getTime(),
-  );
+  // Get all resources from the resources collection
+  const allResources: CollectionEntry<"resources">[] = await getCollection("resources");
 
-  const recentPostPublishedDate = sortedFeedItems[0].data.publishDate.getTime();
+  // Get all legal items from the legal collection
+  const allLegalItems: CollectionEntry<"legal">[] = await getCollection("legal");
 
+  // Get the most recent post published date and calc the index page latest date
+  const recentPostPublishedDate = allFeedIems
+    .sort((a, b) => b.data.publishDate.getTime() - a.data.publishDate.getTime())[0]
+    .data.publishDate.getTime();
   const indexPageLatestDateTimestamp = Math.max(
     recentPostPublishedDate,
     indexPagePublishDate.getTime(),
@@ -23,13 +26,12 @@ export const GET: APIRoute = async ({ site }) => {
   );
   const indexPageLatestDate = new Date(indexPageLatestDateTimestamp).toISOString().substring(0, 10);
 
-  // Get all resources from the resources collection
-  const allResources = await getCollection("resources");
-
-  // Sort resources by publish date (newest first)
-  const sortedResources = allResources.sort(
-    (a, b) => b.data.publishDate.getTime() - a.data.publishDate.getTime(),
-  );
+  const sitemapUrl = (path: string, lastmod: string) => `
+  <url>
+    <loc>${site}${path}</loc>
+    <lastmod>${lastmod}</lastmod>
+  </url>
+  `;
 
   // Generate sitemap XML
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
@@ -38,26 +40,32 @@ export const GET: APIRoute = async ({ site }) => {
     <loc>${site}</loc>
     <lastmod>${indexPageLatestDate}</lastmod>
   </url>
-  ${sortedFeedItems
-    .map(
-      (post) => `
-  <url>
-    <loc>${site}feed/${post.slug}/</loc>
-    <lastmod>${(post.data.contentModifiedDate ?? post.data.publishDate)
-      .toISOString()
-      .substring(0, 10)}</lastmod>
-  </url>`,
+  ${allFeedIems
+    .map((post: CollectionEntry<"feed">) =>
+      sitemapUrl(
+        `feed/${post.slug}/`,
+        (post.data.contentModifiedDate ?? post.data.publishDate).toISOString().substring(0, 10),
+      ),
     )
     .join("")}
-  ${sortedResources
-    .map(
-      (resource) => `
-  <url>
-    <loc>${site}resources/${resource.slug}/</loc>
-    <lastmod>${resource.data.publishDate.toISOString().substring(0, 10)}</lastmod>
-  </url>`,
+  ${allResources
+    .map((resource: CollectionEntry<"resources">) =>
+      sitemapUrl(
+        `resources/${resource.slug}/`,
+        (resource.data.contentModifiedDate ?? resource.data.publishDate)
+          .toISOString()
+          .substring(0, 10),
+      ),
     )
     .join("")}
+    ${allLegalItems
+      .map((legalItem: CollectionEntry<"legal">) =>
+        sitemapUrl(
+          `legal/${legalItem.slug}/`,
+          legalItem.data.contentModifiedDate.toISOString().substring(0, 10),
+        ),
+      )
+      .join("")}
 </urlset>`;
 
   return new Response(sitemap, {
