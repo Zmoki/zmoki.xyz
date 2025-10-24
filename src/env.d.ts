d1f2d80c1dd983676d3aad1ff0ea228d92bce6f9
Zarema Khalilova <zarema.khalilova@gmail.com>
2025-10-25 01:09:49 +0400

    add plausible analytics

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,6 +2,7 @@
 interface ImportMetaEnv {
   readonly PUBLIC_CLOUDFLARE_TURNSTILE_SITE_KEY: string;
   readonly PUBLIC_BREVO_ACCOUNT_ID: string;
+  readonly PUBLIC_PLAUSIBLE_DOMAIN: string;
 }
 
 interface ImportMeta {
